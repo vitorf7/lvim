c9fecc6dd8310795a386cb0a4d9ee29fc2358062
Vitor Faiante <vitorf7@gmail.com>
2022-09-29 16:31:25 +0100

    Some more changes to options

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,5 +1,3 @@
-lvim.colorscheme = "nightfox"
-
 lvim.log.level = "warn"
 lvim.builtin.alpha.active = true
 lvim.builtin.bufferline.active = false
@@ -16,7 +14,7 @@ vim.opt.showtabline = 0
 local options = {
   backup = false,                          -- creates a backup file
   clipboard = "unnamedplus",               -- allows neovim to access the system clipboard
-  cmdheight = 2,                           -- more space in the neovim command line for displaying messages
+  cmdheight = 1,                           -- more space in the neovim command line for displaying messages
   completeopt = { "menuone", "noselect" }, -- mostly just for cmp
   conceallevel = 0,                        -- so that `` is visible in markdown files
   fileencoding = "utf-8",                  -- the encoding written to a file
@@ -32,9 +30,9 @@ local options = {
   splitright = true,                       -- force all vertical splits to go to the right of current window
   swapfile = false,                        -- creates a swapfile
   termguicolors = true,                    -- set term gui colors (most terminals support this)
-  timeoutlen = 100,                        -- time to wait for a mapped sequence to complete (in milliseconds)
+  timeoutlen = 100,                       -- time to wait for a mapped sequence to complete (in milliseconds)
   undofile = true,                         -- enable persistent undo
-  updatetime = 100,                        -- faster completion (4000ms default)
+  updatetime = 100,                       -- faster completion (4000ms default)
   writebackup = false,                     -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
   expandtab = true,                        -- convert tabs to spaces
   shiftwidth = 2,                          -- the number of spaces inserted for each indentation
@@ -43,18 +41,24 @@ local options = {
   number = true,                           -- set numbered lines
   laststatus = 3,
   showcmd = false,
-  ruler = true,
-  relativenumber = false,                  -- set relative numbered lines
+  ruler = false,
+  relativenumber = false,                   -- set relative numbered lines
   numberwidth = 4,                         -- set number column width to 2 {default 4}
   signcolumn = "yes",                      -- always show the sign column, otherwise it would shift the text each time
   wrap = false,                            -- display lines as one long line
-  scrolloff = 8,                           -- is one of my fav
+  scrolloff = 0,
   sidescrolloff = 8,
   guifont = "monospace:h17",               -- the font used in graphical neovim applications
   title = true,
-  colorcolumn = "120"                      -- Line to help with coding best practices
-}
+  -- colorcolumn = "80",
+  -- colorcolumn = "120",
 
+}
+-- vim.g.loaded_netrw = 1
+-- vim.g.loaded_netrwPlugin = 1
+-- vim.opt.fillchars.eob = " "
+-- vim.opt.fillchars = vim.opt.fillchars + "vertleft: "
+-- vim.opt.fillchars = vim.opt.fillchars + "vertright: "
 vim.opt.fillchars = vim.opt.fillchars + 'eob: '
 vim.opt.fillchars:append {
   stl = ' ',
@@ -75,3 +79,4 @@ vim.filetype.add {
     conf = "dosini",
   },
 }
+
